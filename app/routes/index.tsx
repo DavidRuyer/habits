cbbf4472b1a359cd32fd508548413f85e0648d06
David Ruyer <david.ruyer@gmail.com>
2022-08-06 12:15:54 +0200

    DB setup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,41 @@
+import type { Hit } from "@prisma/client";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { db } from "../utils/db.server";
+
+type LoaderData = { hits: Array<Hit> };
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    hits: await db.hit.findMany(),
+  };
+  return json(data);
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  await db.hit.create({ data: {} });
+
+  return redirect("/");
+};
+
 export default function Index() {
+  const data = useLoaderData<LoaderData>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <>
+      <h1>HITS</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.hits.map((hit) => (
+          <li key={hit.id}>
+            {hit.id} - {hit.createdAt}
+          </li>
+        ))}
       </ul>
-    </div>
+      <Form method="post">
+        <button type="submit">Create</button>
+      </Form>
+    </>
   );
 }
---
+++ prisma/migrations/20220806095834_create_hits/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Hit" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Hit_pkey" PRIMARY KEY ("id")
+);
