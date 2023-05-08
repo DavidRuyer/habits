b58ba478961aec70bb6ee7ff20ff39d1da89f5d3
David Ruyer <david.ruyer@gmail.com>
2023-05-08 12:47:30 +0200

    Rework gym exercises

--- app/config/gymConfig.ts
+++ app/config/gymConfig.ts
@@ -1,4 +1,4 @@
-export const WEIGHTS = [25, 27, 32, 36, 41, 45, 50, 55, 60, 65, 70, 73, 75, 78];
+export const WEIGHTS = [25, 27, 32, 36, 39, 41, 45, 50, 55, 60, 65, 68, 73, 78];
 
 export const GAINAGE_WEIGHTS = [30, 45, 60, 90];
 
@@ -25,36 +25,6 @@ export const RATINGS = [
 
 export const EXERCISES = [
   {
-    id: "gainage",
-    name: "Gainage",
-    weights: GAINAGE_WEIGHTS,
-  },
-  {
-    id: "pompes",
-    name: "Pompes",
-    weights: POMPES_WEIGHTS,
-  },
-  {
-    id: "squats",
-    name: "Squats",
-    weights: POMPES_WEIGHTS,
-  },
-  {
-    id: "developpe-couche",
-    name: "Développé Couché",
-    weights: WEIGHTS,
-  },
-  {
-    id: "developpe-horizontal",
-    name: "Développé Horizontal",
-    weights: WEIGHTS,
-  },
-  {
-    id: "tire-assis",
-    name: "Tirage Horizontal",
-    weights: WEIGHTS,
-  },
-  {
     id: "developpe-epaules",
     name: "Développé Vertical",
     weights: WEIGHTS,
@@ -62,6 +32,16 @@ export const EXERCISES = [
   {
     id: "tire-traction",
     name: "Poulie verticale",
+    weights: WEIGHTS,
+  },
+  {
+    id: "developpe-couche",
+    name: "Développé Couché",
+    weights: WEIGHTS,
+  },
+  {
+    id: "tire-assis",
+    name: "Tirage Horizontal",
     weights: WEIGHTS,
   },
   {
@@ -77,11 +57,6 @@ export const EXERCISES = [
   {
     id: "poignees",
     name: "Poulie tiré corde",
-    weights: WEIGHTS,
-  },
-  {
-    id: "curl-corde",
-    name: "Poulie curl",
     weights: WEIGHTS,
   },
   {
